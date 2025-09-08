d16f0022ffb65688147cf675399dc904a2bdbac8
Ravi Kumar <ravibseb96@gmail.com>
2025-09-08 22:29:44 +0530

    Otp varification with email has been done

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,6 @@ import path from "path";
 
 //import routes
 import authRoutes from "./routes/auth.route.js";
-// import productRoutes from "./routes/product.route.js";
-// import cartRoutes from "./routes/cart.route.js";
-// import couponRoutes from "./routes/coupon.route.js";
-// import paymentRoutes from "./routes/payment.route.js";
-// import analyticsRoutes from "./routes/analytics.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -19,31 +14,18 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// const __dirname = path.resolve();
-
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
 // CORS configuration for frontend
-app.use(cors({
-  origin: "http://localhost:5173", // Vite dev server default port
-  credentials: true // Allow cookies to be sent
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Vite dev server default port
+    credentials: true, // Allow cookies to be sent
+  })
+);
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/products", productRoutes);
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/coupons", couponRoutes);
-// app.use("/api/payments", paymentRoutes);
-// app.use("/api/analytics", analyticsRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
